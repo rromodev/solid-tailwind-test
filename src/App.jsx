7e4fbf61d55daa0aa3dcb305b4bd355f111514dc
Raul Romo <rromomxdev@gmail.com>
2023-08-05 18:31:06 -0600

    feat: list render

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, Show } from "solid-js";
+import { createEffect, createSignal, For, Show } from "solid-js";
 
 function App() {
   const [darkMode, setDarkMode] = createSignal(false);
@@ -11,7 +11,12 @@ function App() {
     setDarkMode(!darkMode());
   }
 
-  const [completed, setCompleted] = createSignal(false);
+  const [todos, setTodos] = createSignal([
+    { text: "Abrazar un pinguino", completed: true },
+    { text: "Saludar pinguino", completed: false },
+    { text: "Tomarle foto a un pinguino", completed: false },
+    { text: "Comer con un pinguino", completed: false },
+  ]);
 
   return (
     <div class="w-full h-full min-h-screen flex items-center justify-center dark:bg-gray-600 dark:text-white">
@@ -27,20 +32,31 @@ function App() {
         <input class="border dark:text-black" type="text" />
         <button class="px-2 border">Add</button>
         <ul>
-          <li>
-            <input type="checkbox" checked />
-            <span onClick={() => setCompleted(!completed())}>
-              <Show when={completed()} fallback={"Abrazar Pinguino"}>
-                <s style="pointer-events: none">Abrazar Pinguino</s>
-              </Show>
-            </span>
-            <button>❌</button>
-          </li>
-          <li>
-            <input type="checkbox" />
-            <span>Saludar Pinguino</span>
-            <button>❌</button>
-          </li>
+          <For each={todos()} fallback={"No hay elementos"}>
+            {(todo, index) => (
+              <li>
+                <input
+                  type="checkbox"
+                  checked={(console.log("test"), todo.completed)}
+                  onChange={() => {
+                    setTodos((oldTodos) =>
+                      oldTodos.map((t, i) => {
+                        return i === index()
+                          ? { ...t, completed: !t.completed }
+                          : t;
+                      })
+                    );
+                  }}
+                />
+                <span>
+                  <Show when={todo.completed} fallback={todo.text}>
+                    <s style="pointer-events: none">{todo.text}</s>
+                  </Show>
+                </span>
+                <button>❌</button>
+              </li>
+            )}
+          </For>
         </ul>
         <p class="text-sm mt-4">Completed count: {0}</p>
       </div>
